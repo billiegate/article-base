d9e7f8c028ad6c3f4619673cc876dd980ca55090
MacBook Pro <billiegate8@gmail.com>
2023-05-16 13:12:49 +0100

    docs init

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,23 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 /**
- * @OA\Info(title="Article Base", version="0.1")
+ * @OA\Info(
+ *      version="1.0.0",
+ *      x={
+ *          "logo": {
+ *              "url": "https://via.placeholder.com/190x90.png?text=L5-Swagger"
+ *          }
+ *      },
+ *      title="Article Base",
+ *      description="An article CRUD project",
+ *      @OA\Contact(
+ *          email="afolabitope@ymail.com"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
  */
 class Controller extends BaseController
 {
